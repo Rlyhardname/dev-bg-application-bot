20449055bd50ecc71bab30822129028ec944bc23
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-11-11 12:00:52 +0200

    -Moved the static START_URL from context to NavigationConfig, to allow more flexible configuration in the future via graphical interfaces.

--- src/main/java/com/dimitrovsolutions/entryPoint.java
+++ src/main/java/com/dimitrovsolutions/entryPoint.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 public class entryPoint {
     public static void main(String[] args) {
-        Orchestrator.start(new NavigationConfig("https://dev.bg", new ArrayDeque<>(List.of(
+        Orchestrator.start(new NavigationConfig(
+                "https://dev.bg/company/jobs/java/?_seniority=intern",
+                "https://dev.bg", new ArrayDeque<>(List.of(
                 "https://dev.bg/company/jobs/",
                 "https://dev.bg/company/jobs/java/",
                 "https://dev.bg/company/jobs/java/?_seniority=intern"))
