64896b6063ad993401023163abb51a3fcd4e8ef8
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-12-24 18:23:11 +0200

    - add java doc - bugfix - add wait on dropdown to avoid element not being loaded fast enough - add ThreadSleep before sending new application to avoid spamming employers before kinks are ironed out.

--- src/main/java/com/dimitrovsolutions/io/automation/SeleniumFacade.java
+++ src/main/java/com/dimitrovsolutions/io/automation/SeleniumFacade.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,6 +19,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -109,6 +112,7 @@ public class SeleniumFacade implements Destructor {
 
                 clickComboBox();
 
+                Thread.sleep(60000);
                 clickSecondApplicationButton();
 
                 if (isPageUrlSame(job.url())) {
@@ -183,6 +187,9 @@ public class SeleniumFacade implements Destructor {
         Thread.sleep(1500);
     }
 
+    /**
+     * Open url of job being applied to.
+     */
     private Job openJobListing(Job job) throws InterruptedException {
         driver.get(job.url());
         Thread.sleep(1000);
@@ -190,22 +197,37 @@ public class SeleniumFacade implements Destructor {
         return job;
     }
 
+    /**
+     * Click apply button triggering application form to popup.
+     */
     private void clickFirstApplicationButton() throws InterruptedException {
         WebElement apply = driver.findElement(By.className("single-job-apply-button"));
         apply.click();
         Thread.sleep(1000);
     }
 
+    /**
+     * Click dropdown with my files, triggering list of my files to show.
+     */
     private void clickDropDown() {
         WebElement clickDropDown = driver.findElement(By.className("upload-from-profile"));
         clickDropDown.click();
     }
 
+    /**
+     * Choose combo box with cv file(should only have one cv file).
+     */
     private void clickComboBox() {
-        WebElement checkBox = driver.findElement(By.className("wpcf7-user_files"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement checkBox = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        driver.findElement(By.className("wpcf7-user_files"))));
         checkBox.click();
     }
 
+    /**
+     * Click second application button triggering CV being sent.
+     */
     private void clickSecondApplicationButton() {
         List<WebElement> listOfApply = driver.findElements(By.className("wpcf7-submit"));
         // Button for sending final application
@@ -213,10 +235,16 @@ public class SeleniumFacade implements Destructor {
         finalApply.click();
     }
 
+    /**
+     * Check if we've been redirected somewhere, should stay on the same page after application
+     */
     private boolean isPageUrlSame(String url) {
         return url.equals(driver.getCurrentUrl());
     }
 
+    /**
+     * Log the current page.
+     */
     private void logPageContentAfterSendingApplication(HttpClientFacade client) {
         HttpRequest request = client.getRequest(driver.getCurrentUrl());
         HttpResponse<String> response = client.send(request);
@@ -224,12 +252,18 @@ public class SeleniumFacade implements Destructor {
         logger.log(Level.CONFIG, body);
     }
 
+    /**
+     * Destroy the current instance of the driver, closing the browser window.
+     */
     public void quitDriver() {
         if (driver != null) {
             driver.quit();
         }
     }
 
+    /**
+     * Used in finally method of Orchestrator.
+     */
     @Override
     public void tearDown() {
         logger.log(Level.INFO, "Selenium logger teardown at: " + LocalDateTime.now());
